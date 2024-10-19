354af7ceeb8b3f1f91b95ae541f29d0cb5802cfd
Perezhir000 <137489079+Perezhir000@users.noreply.github.com>
2024-10-19 14:44:29 -0600

    created Verilog code for a dual port ram
    RAM can write to address on high write enable and on posedge and can read from RAM on negedge with provided memory address. Test bench still needed

--- RAM.v
+++ RAM.v
@@ -1,1 +1,25 @@
-//RAM Module
+// Dual Port RAM for FFT
+module RAM #(parameter DATA_BIT_WIDTH = 32, ADDR_BIT_WIDTH = 32)
+	(
+		input [ADDR_BIT_WIDTH -1:0] write_addr, read_addr,
+		input [DATA_BIT_WIDTH - 1: 0] data_in,
+		input write_en, clk,
+		output reg [DATA_BIT_WIDTH - 1:0] data_out
+	);
+	
+	// declare ram
+	reg [DATA_BIT_WIDTH - 1: 0] ram [(1<<ADDR_BIT_WIDTH) - 1: 0];
+	
+	assign nclk = ~clk;
+	// write to memory on posedge.
+	always@(posedge clk)
+	begin
+		if(write_en)
+			ram[write_addr] <= data_in;
+		end
+	// read from memory on negedge of clk or posedge of nclk.
+	always@(posedge nclk)
+	begin
+		data_out <= ram[read_addr];
+	end
+endmodule
