c7dfee42bf105da46cd5d473e683f4b75cb6e7b8
Perezhir000 <137489079+Perezhir000@users.noreply.github.com>
2024-10-20 19:20:04 -0600

    Updated RAM.v to correct 64 byte RAM size

--- RAM.v
+++ RAM.v
@@ -1,8 +1,8 @@
 // Dual Port RAM for FFT
-module RAM #(parameter DATA_BIT_WIDTH = 32, ADDR_BIT_WIDTH = 32)
+module RAM #(parameter DATA_BIT_WIDTH = 32, ADDR_BIT_WIDTH = 16)
 	(
 		input [ADDR_BIT_WIDTH -1:0] write_addr, read_addr,
-		input [DATA_BIT_WIDTH - 1: 0] data_in,
+		input [DATA_BIT_WIDTH - 1:0] data_in,
 		input write_en, clk,
 		output reg [DATA_BIT_WIDTH - 1:0] data_out
 	);
